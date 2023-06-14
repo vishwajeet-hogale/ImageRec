10cd17d4edf549b7ef750328a7d65f574773a329
Hogale <vhogale@apac.corpdir.net>
2023-06-14 10:22:45 +0530

    Adding new requirement

--- app.py
+++ app.py
@@ -33,6 +33,28 @@ def predict():
         return render_template("index.html",all_image_paths = pred_list,keys=FaceDict.keys())
     return render_template("index.html",keys = keys)
 
+@app.route("/predict1",methods = ["POST","GET"])
+def predict1():
+    global FaceDict
+    # if not FaceDict :
+    #     FaceDict = services.train()
+    if request.method == "POST":
+        name = request.form['name']
+        # return {"Hello":all_image_paths = FaceDict[request.form["name"]]}
+        pred_list = None
+        try:
+            pred_list = FaceDict[str(name)]
+        except:
+            FaceDict = services.load_face_dict("./Output")
+            pred_list = FaceDict[name]
+
+            # services.copy_files(name,[i.split("/")[-1] for i in pred_list])
+        pred_list = [i.split("/")[-1] for i in pred_list]
+        services.copy_files(name,pred_list)
+        return render_template("index.html",all_image_paths = pred_list,keys=FaceDict.keys())
+    return render_template("index.html",keys = keys)
+
+
 if __name__ == "__main__":
     # global FaceDict
     # global keys
